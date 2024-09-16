1f8124c5107a6212a6eee951f5213fea4a24a33a
Magnesium- <yyong007@outlook.com>
2024-09-16 09:49:28 +0800

    ✨ dashboard

--- frontend/src/routes/admin/dashboard/index.tsx
+++ frontend/src/routes/admin/dashboard/index.tsx
@@ -1,5 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { PageContainer, ProCard } from "@ant-design/pro-components";
 
-export const Route = createFileRoute('/admin/dashboard/')({
-  component: () => <div>Hello /_admin/dashboard!</div>
-})
+import { LoginIn } from "@/components/Admin/Dashboard/login-in";
+import { SignIn } from "@/components/Admin/Dashboard/sign-in";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/admin/dashboard/")({
+  component: DashboardRoute,
+});
+
+export function DashboardRoute() {
+  const userInfo = {};
+  const _data = { data: {}, isLoading: false };
+  const { data, isLoading } = _data || {};
+
+  return (
+    <PageContainer loading={isLoading}>
+      <ProCard>
+        <ProCard>
+          <div className="flex justify-between">
+            <LoginIn data={data} userInfo={userInfo} />
+            <SignIn data={data} />
+          </div>
+        </ProCard>
+      </ProCard>
+    </PageContainer>
+  );
+}

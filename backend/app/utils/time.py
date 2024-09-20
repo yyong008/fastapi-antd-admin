67fe03462abeb76226b3958865cd46d1f5eb3cb4
Magnesium- <yyong007@outlook.com>
2024-09-20 18:09:51 +0800

    ⚰️ remove print

--- backend/app/utils/time.py
+++ backend/app/utils/time.py
@@ -4,5 +4,3 @@ def get_today_time():
     today_midnight = datetime.combine(datetime.today(), datetime.min.time())
     tomorrow_midnight = today_midnight + timedelta(days=1)
     return today_midnight, tomorrow_midnight
-
-print(get_today_time())

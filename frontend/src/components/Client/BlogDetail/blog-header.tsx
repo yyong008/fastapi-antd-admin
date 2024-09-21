e42a85b5d4edabb6acb51efce294574fda1eeec0
Magnesium- <yyong007@outlook.com>
2024-09-21 22:59:16 +0800

    ✨ blogs details styles

--- frontend/src/components/Client/BlogDetail/blog-header.tsx
+++ frontend/src/components/Client/BlogDetail/blog-header.tsx
@@ -11,9 +11,9 @@ type BlogHeaderProps = {
 
 export function BlogHeader({ blog }: BlogHeaderProps) {
   return (
-    <div className="flex flex-col pt-[140px] w-[40vw] h-[80vh]">
+    <div className="flex flex-col w-[40vw]">
       <div className="text-[30px]">{blog.title}</div>
-      <div className="flex text-gray-500 text-[14px] mt-[10px]">
+      <div className="flex text-gray-500 text-[14px]">
         <div className="mr-[10px]">作者：{blog.author}</div>
         <div className="mr-[10px]">来源：{blog.source}</div>
         <div>发布时间：{dayjs(blog.publishedAt).format("YYYY-MM-DD")}</div>
--- frontend/src/routes/_client/blog_.$id.tsx
+++ frontend/src/routes/_client/blog_.$id.tsx
@@ -6,20 +6,22 @@ import { getBlogById } from "@/apis/client/blog";
 export const Route = createFileRoute("/_client/blog/$id")({
   component: BlogDetailRoute,
   async loader({ params }) {
-    const result: any = await getBlogById(Number(params.id))
-    if(result && result.code === 0) {
-      return result.data
+    const result: any = await getBlogById(Number(params.id));
+    if (result && result.code === 0) {
+      return result.data;
     }
-  }
+  },
 });
 
 export function BlogDetailRoute() {
   const _data = Route.useLoaderData();
   const blog = _data;
   return (
-    <div className="flex flex-col  w-[40vw] h-[80vh]">
-      <BlogHeader blog={blog} />
-      <BlogContent content={blog.content} />
+    <div className="flex flex-col pt-[140px]  w-[70vw] min-h-[80vh] pb-[40px]">
+      <div className="bg-white p-[20px] font-sans rounded-sm text-base/loose pb-[60px]">
+        <BlogHeader blog={blog} />
+        <BlogContent content={blog.content} />
+      </div>
     </div>
   );
 }

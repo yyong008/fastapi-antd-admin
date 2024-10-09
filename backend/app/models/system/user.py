02db78f82c4e2a5c8adcf6dbbeeaa89062e91bb1
Magnesium- <yyong007@outlook.com>
2024-10-10 03:05:39 +0800

    fix: system user column created_at updated_at

--- backend/app/models/system/user.py
+++ backend/app/models/system/user.py
@@ -40,8 +40,8 @@ class User(Base):
     phone = Column(String, nullable=True)
     remark = Column(String, nullable=True)
     status = Column(Integer, nullable=True)
-    createdAt = Column(DateTime(timezone=True), server_default=func.now())
-    updatedAt = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     department_id = Column(
         Integer,
         ForeignKey("sys_department.id", ondelete="CASCADE", onupdate="CASCADE"),

369c1ca0dc6d7516948dbe47cb06e69bbbbf1376
Magnesium- <yyong007@outlook.com>
2024-10-10 00:46:22 +0800

    ✨ profile link table and profile_link_category  table add created_at updated_at

--- backend/app/models/profile/link.py
+++ backend/app/models/profile/link.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, func
 from sqlalchemy.orm import relationship
 from app.db.client import Base
 
@@ -16,7 +16,8 @@ class Link(Base):
         ForeignKey("profile_link_category.id", ondelete="RESTRICT", onupdate="CASCADE"),
         nullable=False,
     )
-
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
     category = relationship("LinkCategory", back_populates="links")  # 反向关系
 
 
@@ -27,5 +28,6 @@ class LinkCategory(Base):
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
     user_id = Column(Integer, nullable=False)  # 上传用户，可能需要外键根据需求
-
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
     links = relationship("Link", back_populates="category")  # 反向关系

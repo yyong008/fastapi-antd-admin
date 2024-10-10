dd4fd90df08294ba92e2817f9e3695080399420b
Magnesium- <yyong007@outlook.com>
2024-10-11 01:52:42 +0800

    🎉 login_at columns

--- frontend/src/components/Admin/System/Monitor/LoginLog/create-columns.tsx
+++ frontend/src/components/Admin/System/Monitor/LoginLog/create-columns.tsx
@@ -1,3 +1,4 @@
+import { Tag } from "antd";
 import { formatDate } from "@/utils/utils";
 
 export const createColumns = () => [
@@ -5,6 +6,17 @@ export const createColumns = () => [
     dataIndex: "name",
     title: "用户名",
     ellipsis: true,
+    render(_: any, record: any) {
+      return <Tag color="blue">{record.name}</Tag>;
+    },
+  },
+  {
+    dataIndex: "login_at",
+    title: "登录时间",
+    ellipsis: true,
+    render(_: any, record: any) {
+      return <Tag  color="purple">{record.login_at ? formatDate(record.login_at) : "-"}</Tag>;
+    },
   },
   {
     dataIndex: "ip",
@@ -26,12 +38,5 @@ export const createColumns = () => [
     title: "浏览器",
     ellipsis: true,
   },
-  {
-    dataIndex: "loginAt",
-    title: "登录时间",
-    ellipsis: true,
-    render(_: any, record: any) {
-      return <div>{record.login_at ? formatDate(record.login_at) : "-"}</div>;
-    },
-  },
+
 ];
